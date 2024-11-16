2ee07a66449d78876af932b058a14f5e99fced28
Basir Dahy <basirdahy@yahoo.com>
2024-11-15 23:30:11 -0800

    added file

---
+++ src/basir/Basir01.java
@@ -0,0 +1,26 @@
+package basir;
+
+public class Basir01 {
+
+    public static void main(String[] args) {
+
+        int odd = 5;
+        int even = 6;
+
+        if(odd % 2 == 0){
+            System.out.println("Odd");
+        }else{
+            System.out.println("Even");
+        }
+
+        if(even %2 == 0){
+            System.out.println("Odd");
+        }else {
+            System.out.println("Even");
+        }
+
+        }
+
+
+    }
+
