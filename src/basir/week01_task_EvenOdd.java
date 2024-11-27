dd92cbb98ae132d4ddd8328fa79d68b73c50db44
Basir Dahy <basirdahy@yahoo.com>
2024-11-27 10:35:00 -0800

    task01_solution

--- src/basir/week01_task_EvenOdd.java
+++ src/basir/week01_task_EvenOdd.java
@@ -4,22 +4,19 @@ public class week01_task_EvenOdd {
 
     public static void main(String[] args) {
 
-        int odd = 5;
-        int even = 6;
-
-        if(odd % 2 == 0){
-            System.out.println("Odd");
-        }else{
-            System.out.println("Even");
-        }
-
-        if(even %2 == 0){
-            System.out.println("Odd");
-        }else {
-            System.out.println("Even");
-        }
-
+        OddorEven(11);
+        System.out.println(OddorEven(6));
     }
 
+    public static String OddorEven(int number) {
+        if (number % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
     }
+}
+
+
+
 
