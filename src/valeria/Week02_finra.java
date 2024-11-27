165c510512c9b181d2da5db0c0cab43fff3c34fa
valeriabernstein <rusuvaleria1@gmail.com>
2024-11-27 15:54:14 -0800

    FINRA TASK REVIEWED

--- src/valeria/Week02_finra.java
+++ src/valeria/Week02_finra.java
@@ -3,12 +3,14 @@ package valeria;
 public class Week02_finra {
     public static void main(String[] args) {
         for (int i = 1; i <= 30; i++) {
-            if (i % 3 == 0) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
                 System.out.println("FIN");
             } else if (i % 5 == 0) {
                 System.out.println("RA");
-            } else {
-                System.out.println("FINRA");
+            } else{
+                System.out.println(i);
             }
 
         }
