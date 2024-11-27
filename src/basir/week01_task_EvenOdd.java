981e6cf80dad69538b92dfc564a33c64f8fcaf4e
Basir Dahy <basirdahy@yahoo.com>
2024-11-26 22:53:55 -0800

    task2 solution

---
+++ src/basir/week01_task_EvenOdd.java
@@ -0,0 +1,25 @@
+package basir;
+
+public class week01_task_EvenOdd {
+
+    public static void main(String[] args) {
+
+        int odd = 5;
+        int even = 6;
+
+        if(odd % 2 == 0){
+            System.out.println("Odd");
+        }else{
+            System.out.println("Even");
+        }
+
+        if(even %2 == 0){
+            System.out.println("Odd");
+        }else {
+            System.out.println("Even");
+        }
+
+    }
+
+    }
+
