cc127454b9c9eab5bb0777c2bef1bdf9a0e810da
alireza8260 <alireza8260@gmail.com>
2025-02-01 11:32:24 -0800

    Create Method for Week09_PasswordValidation

---
+++ src/alireza/Week09_PasswordValidation.java
@@ -0,0 +1,58 @@
+package alireza;
+
+import java.util.Scanner;
+
+public class Week09_PasswordValidation {
+    public static void main(String[] args) {
+        password();
+
+    }
+
+    public static boolean password() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter your Password : ");
+        String password = scanner.next();
+        boolean result = true;
+        Boolean hasUpperCase = true;
+        Boolean hasLowerCase = true;
+        Boolean hasDigit = true;
+        Boolean hasMoreThan5Characters = true;
+        Boolean hasSpecialChar = true;
+
+        if (!password.matches(".*[A-Z].*")) {
+            hasUpperCase = false;
+            System.out.println("password should contains at least one UpperCase character ");
+
+        }
+        if (!password.matches(".*[a-z].*")) {
+            hasLowerCase = false;
+            System.out.println("password should contains at least one LowerCase Character ");
+        }
+        if (!password.matches(".*[0-9].*")) {
+            hasDigit = false;
+            System.out.println("password should contains at least one Digit ");
+
+        }
+        if (password.length() < 6 || password.contains(" ")) {
+            hasMoreThan5Characters = false;
+            System.out.println("Password should be 6 or more characters and do not have any space");
+
+        }
+        if (!password.matches(".*[^a-zA-Z0-9].*")) {
+            hasSpecialChar = false;
+            System.out.println("password should contain at least one Special characters");
+        }
+
+
+        if (hasUpperCase && hasLowerCase && hasDigit && hasMoreThan5Characters && hasSpecialChar) {
+            result = true;
+            System.out.println("result = " + result);
+            System.out.println("Great, your password match all conditions");
+        } else {
+            result = false;
+            System.out.println("result = " + result);
+            System.out.println("Sorry, your password is not match the conditions");
+        }
+        return result;
+    }
+}
